3092ad478880beb5b0e7edd9624ac75df179198c
Ceri Williams <github@incoming-email.co.uk>
2023-11-16 18:58:53 +0000

    Initial, barebones SEP module (#2)
    * Added placeholders with minimal content for SEP
    * Added sep.authz.casdoor for Casdoor authorization
    * Added sample config for running SEP
    * Updated Makefile to add format task
    * Added MANIFEST.in for module builds
    * Updated pyproject.toml to configure tooling and resolve the version
    dynamically from the source code
    
    Additional fixes/improvements for workflow:
    * Added ready_for_review to CI types
    * Added working-directory to step
    * Added working-directory to step
    * Refactored audit tasks
    * Added toml and bandit[toml] to bandit task

--- Makefile
+++ Makefile
@@ -21,13 +21,21 @@ build: prep
 	@python3 -m build --wheel --outdir build
 	@rm -rf sep.egg-info
 
+format:
+	@ruff format .
+
 lint:
 	@pip install --upgrade ruff
 	@ruff check .
 
 audit: export TMPDIR=build/tmp
-audit: prep lint
-	@pip install --upgrade --quiet bandit pip-tools pip-audit
+audit: prep lint bandit pip-audit
+
+pip-audit:
+	@pip install --upgrade --quiet pip-tools pip-audit
 	@pip-compile --no-strip-extras --generate-hashes pyproject.toml
-	@bandit -r src
 	@pip-audit --verbose --progress-spinner=off --require-hashes -r requirements.txt
+
+bandit:
+	@pip install --upgrade --quiet bandit toml 'bandit[toml]'
+	@bandit -c pyproject.toml -r src
---
+++ src/sep/authz/casdoor.py
@@ -0,0 +1,97 @@
+"""
+Casdoor authentication and authorization
+"""
+from collections import namedtuple
+from http import HTTPStatus
+import json
+from secrets import token_hex
+from typing import (
+    Any,
+    Dict,
+    Optional,
+    cast,
+)
+
+from jwt.exceptions import DecodeError
+from tornado.auth import OAuth2Mixin
+from tornado.log import app_log
+from tornado.web import (
+    HTTPError,
+    RequestHandler,
+)
+
+__all__ = ["CasdoorOAuth2Mixin"]
+
+AuthzConfig = namedtuple(
+    "AuthzConfig", ["CASDOOR_COOKIE", "CASDOOR_SDK", "CASDOOR_SDK_SYNC", "REDIRECT_URI", "SECRET_KEY", "SESSION_COOKIE"]
+)
+
+
+class CasdoorOAuth2Mixin(OAuth2Mixin):
+    """Casdoor authentication using OAuth2"""
+
+    # The following are not used, unlike other OAuth2Mixin classes:
+    # _OAUTH_AUTHORIZE_URL
+    # _OAUTH_ACCESS_TOKEN_URL
+    # _OAUTH_USERINFO_URL
+    # _OAUTH_NO_CALLBACKS
+    # _OAUTH_SETTINGS_KEY
+
+    cfg: namedtuple
+
+    def get_casdoor_oauth_settings(self) -> AuthzConfig:
+        """Get the Casdoor OAuth 2.0 credentials
+
+        This is a mixin and so the configuration is set by the handler that uses
+        CasdoorOAuth2Mixin.
+
+        :raises LookupError: when "authz" is not present in self.cfg
+        :returns a readonly copy of the OAuth2 settings
+        :rtype AuthzConfig
+        """
+        if not hasattr(self.cfg, "authz"):
+            raise LookupError("authz is not present in the configuration")
+        return self.cfg.authz
+
+    async def get_authenticated_user(self, code: str) -> Dict[str, Any]:
+        """Handle login with Casdoor
+
+        :param code: the code sent via the callback request from Casdoor
+        :type code: str
+        :raises HTTPError: when the user authentication fails
+        :returns the user's details
+        :rtype dict
+        """
+        app_log.debug("Checking authenticated user")
+        handler = cast(RequestHandler, self)
+        token = await self.cfg.authz.CASDOOR_SDK.get_oauth_token(code)
+
+        try:
+            user = self.cfg.authz.CASDOOR_SDK.parse_jwt_token(token.get("access_token"))
+        except DecodeError as err:
+            raise HTTPError(status_code=HTTPStatus.UNAUTHORIZED) from err
+        # TODO: limit duration
+        handler.set_signed_cookie(
+            name=self.cfg.authz.CASDOOR_COOKIE,
+            httponly=True,
+            expires_days=None,
+            samesite="strict",
+            secure=self.cfg.authz.REDIRECT_URI.startswith("https://"),
+            value=json.dumps(user),
+        )
+        return user
+
+    def generate_session(self, data: Optional[Dict] = None):
+        """Generate a session, with optional data"""
+        handler = cast(RequestHandler, self)
+        cookie_data = {"id": token_hex(41), "next": handler.request.uri}
+        if isinstance(data, dict):
+            cookie_data.update({k: v for k, v in data.items() if k not in ["id"]})
+        handler.set_signed_cookie(
+            name=self.cfg.authz.SESSION_COOKIE,
+            httponly=True,
+            expires_days=None,
+            samesite="strict",
+            secure=self.cfg.authz.REDIRECT_URI.startswith("https://"),
+            value=json.dumps(cookie_data),
+        )
