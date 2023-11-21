b6f48578479e927a127439018e57d518264faa35
cezmunsta <github@incoming-email.co.uk>
2023-11-21 17:03:06 +0000

    Added CRUD endpoints for tasks API

--- src/sep/tasks/api/__init__.py
+++ src/sep/tasks/api/__init__.py
@@ -1,10 +1,18 @@
 """
 Tasks API
 """
+from datetime import (
+    datetime,
+    timezone,
+)
+from http import HTTPStatus
 import logging
 from os import getenv
 from secrets import token_hex
-from typing import List
+from typing import (
+    cast,
+    List,
+)
 
 from fastapi import (
     Depends,
@@ -12,8 +20,12 @@ from fastapi import (
     HTTPException,
 )
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import RedirectResponse
-from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from sqlalchemy import (
+    event,
+    null,
+)
+from sqlalchemy.engine import Engine
 from starlette.middleware.sessions import SessionMiddleware
 
 from .models import (
@@ -66,12 +78,67 @@ async def shutdown():
     await database.disconnect()
 
 
+@event.listens_for(Engine, "connect")
+async def prepare_connection(connection, record):
+    """Prepare the connection"""
+    await sep.core.db.prepare_connection(connection, record)
+
+
 @app.get(path="/", response_model=List[Task])
 async def list_reports():
-    """
+    """List all tasks
 
     :return:
     """
     app.log.debug("Listing reports")
-    query = tasks.select()
+    query = tasks.select().where(tasks.c.deleted_at == null())
     return await database.fetch_all(query)
+
+
+@app.delete(path="/{task}", response_class=JSONResponse)
+async def delete_task(task: str):
+    """Deleta a task
+
+    :param task:
+    :raises HTTPException: when the task does not exist
+    :return: status message
+    """
+    app.log.debug("Deleting task %s", task)
+    query = tasks.select().where(tasks.c.name == task, tasks.c.deleted_at == null())
+    current_task = await database.fetch_one(query)
+    if current_task is None:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST)
+    deleted_at = datetime.now(tz=timezone.utc)
+    affected_rows = await database.execute(
+        tasks.update().where(tasks.c.id == current_task[0]).values(
+            deleted_at=deleted_at
+        )
+    )
+    if affected_rows != 1:
+        return {"error": f"failed to delete task {current_task[0]}"}
+    return {"id": current_task[0], "deleted": True}
+
+
+@app.get(path="/{task}", response_model=Task)
+async def get_task(task: str):
+    """Retrieve a task
+
+    :param task:
+    :return:
+    """
+    app.log.debug("Requesting task %s", task)
+    query = tasks.select().where(tasks.c.name == task)
+    return await database.fetch_one(query)
+
+
+@app.post(path="/", response_model=Task)
+async def create_task(task: TaskBaseModel):
+    """Create a new task
+
+    :param task:
+    :return:
+    """
+    app.log.debug("Creating task %s", task.name)
+    query = tasks.insert().values(**vars(task))
+    last_record_id = await database.execute(query)
+    return {**task.model_dump(), "id": last_record_id}
