5777253193de5cb9fd98a91074128c5f1daa2e3b
cezmunsta <github@incoming-email.co.uk>
2023-11-22 14:58:09 +0000

    Tweaked utils template functions
    * Changed matching rules to use the app directory instead of __file__
      directory
    * Added option to resolve a path in the template dirs
    * Updated render_template to raise a 404 if the template is unusable

--- src/sep/core/utils.py
+++ src/sep/core/utils.py
@@ -1,15 +1,17 @@
 """
 Utility library
 """
+from http import HTTPStatus
 import logging
 import os.path
-from sys import modules
+from sys import argv
 from typing import Union
 
 from tornado.template import (
     Loader,
     Template,
 )
+from tornado.web import HTTPError
 
 LOG_FORMAT = "%(asctime)s %(levelname)s:%(name)s: PID<%(process)d> %(module)s.%(funcName)s - %(message)s"
 
@@ -36,15 +38,19 @@ def get_template(template_name: str, template_dirs: list) -> Union[Template, Non
     :return:
     """
     for template_dir in template_dirs:
+        match template_dir.startswith("#resolve#"):
+            case True:
+                template_dir = os.path.abspath(template_dir.replace("#resolve#", ""))
+
         match template_dir:
-            case "#filedir":
-                _dir = os.path.dirname(__file__)
-            case "#filedir+templates":
-                _dir = os.path.join(os.path.dirname(__file__), "templates")
-            case "#filedir..templates":
-                _dir = os.path.join(os.path.dirname(__file__), "..", "templates")
-            #case "#moduledir":
-            #    _dir = dir(modules[__name__])
+            case "#appdir":
+                _dir = os.path.dirname(argv[0])
+            case "#appdir+templates":
+                _dir = os.path.join(os.path.dirname(argv[0]), "templates")
+            case "#appdir..templates":
+                _dir = os.path.join(os.path.dirname(argv[0]), "..", "templates")
+            case "#appdir....templates":
+                _dir = os.path.join(os.path.dirname(argv[0]), "..", "..", "templates")
             case _:
                 _dir = template_dir
         loader = Loader(_dir)
@@ -60,8 +66,9 @@ def render_template(template: Template, **kwargs):
 
     :param template:
     :param kwargs:
+    :raises tornado.web.HTTPError: when the template is unusable
     :return:
     """
     if template is None:
-        return "Page not found"
+        raise HTTPError(status_code=HTTPStatus.NOT_FOUND)
     return template.generate(**kwargs)
