ed98b4fd7ba5d393def8f199c5a378f43d8a666b
cezmunsta <github@incoming-email.co.uk>
2023-12-13 12:45:12 +0000

    Basic inventory
    API to read and store the inventory from a remote source. This is
    currently limited to PMM.
    
    * Added sep.inventory.api along with data models. The inventory is
      initialised from the remote source when the local data is emtpy.
      Additionally, the inventory can be refreshed by request. There is
      partial support to create and update items in the inventory directly
      via the API, only affecting local storage, which may be overwritten if
      changing an item that is present in the remote source

---
+++ src/sep/inventory/api/__init__.py
@@ -0,0 +1,165 @@
+"""
+Inventory API
+"""
+import logging
+from os import getenv
+from secrets import token_hex
+from typing import (
+    List,
+)
+
+from fastapi import (
+    BackgroundTasks,
+    FastAPI,
+)
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from sqlalchemy import (
+    event,
+    null,
+)
+from sqlalchemy.engine import Engine
+from starlette.middleware.sessions import SessionMiddleware
+
+from .models import (
+    inventory,
+    InventoryItem,
+    InventoryItemBaseModel,
+    InventoryItemData,
+    InventoryItemService,
+    INVENTORY_BACKEND_MAP,
+    INVENTORY_CREDENTIAL_SOURCE_MAP,
+)
+from .pmm import InventorySource
+from sep.authz.casdoor import SESSION_TOKEN_LENGTH
+import sep.core.db
+from sep.core.utils import get_logger
+
+DEFAULT_DATABASE_DSN = f"{sep.core.db.DEFAULT_DATABASE_DSN}/inventory.db"
+DEFAULT_ORIGINS = "http://localhost:8000,http://127.0.0.1:8000"
+
+DATABASE_URL = getenv("INVENTORY_DATABASE_URL", DEFAULT_DATABASE_DSN)
+ORIGINS = getenv("INVENTORY_ORIGINS", DEFAULT_ORIGINS).split(",")
+
+database = sep.core.db.get_database(DATABASE_URL)
+database.metadata = sep.core.db.get_metadata()
+database.engine = sep.core.db.get_engine(DATABASE_URL, connect_args=sep.core.db.DEFAULT_DATABASE_CONNECT_ARGS)
+
+app = FastAPI()
+app.log = get_logger("inventory-api", level=logging.DEBUG)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=token_hex(SESSION_TOKEN_LENGTH),
+    session_cookie="fastapi-session",
+)
+
+
+async def lookup_inventory(source: str = "pmm"):
+    """Lookup the inventory from a remote source
+
+    :param source:
+    :return:
+    """
+    app.log.debug("Checking remote source")
+    inventory_source = None
+    match source:
+        case "pmm":
+            inventory_source = InventorySource(uri="https://localhost:8443", verify_tls=False)
+        case "_":
+            raise NotImplementedError(f"{source} is not supported")
+    # TODO: this needs to be more intelligent for updates
+    for _, node in inventory_source.nodes.items():
+        app.log.debug("Checking for node %s", node.name)
+        exists = await database.fetch_all(inventory.select().where(inventory.c.node_id == node.id))
+
+        services = []
+        for svc in node.services:
+            services.append(InventoryItemService(**svc.to_dict()))
+        item_data = InventoryItemData(name=node.name, services=services)
+        model = InventoryItemBaseModel(
+            name=node.name,
+            node_id=node.id,
+            data=item_data,
+            backend=INVENTORY_BACKEND_MAP["pmm"],
+            credential_source=INVENTORY_CREDENTIAL_SOURCE_MAP["default"],
+        )
+        if not exists:
+            await create_item(model)
+        else:
+            item_id = exists[0][0]
+            await update_item(item_id, model)
+
+
+@app.on_event("startup")
+async def startup():
+    """Prepare the database and application"""
+    inventory.to_metadata(database.metadata)
+    await sep.core.db.startup(database)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    """Perform a clean shutdown"""
+    await database.disconnect()
+
+
+@event.listens_for(Engine, "connect")
+def prepare_connection(connection, record):
+    """Prepare the connection"""
+    sep.core.db.prepare_connection(connection, record)
+
+
+@app.get(path="/", response_model=List[InventoryItem])
+async def list_inventory(background_tasks: BackgroundTasks):
+    """List the inventory content
+
+    :return:
+    """
+    app.log.debug("Listing inventory")
+    query = inventory.select().where(inventory.c.deleted_at == null())
+    results = await database.fetch_all(query)
+    if not results:
+        background_tasks.add_task(lookup_inventory, "pmm")
+    return results
+
+
+@app.get(path="/refresh", response_class=JSONResponse)
+async def refresh_inventory(background_tasks: BackgroundTasks):
+    """Refresh the inventory content
+
+    :return:
+    """
+    app.log.debug("Refreshing the inventory")
+    background_tasks.add_task(lookup_inventory, "pmm")
+    return {"status": "success"}
+
+
+@app.post(path="/", response_model=InventoryItem)
+async def create_item(item: InventoryItemBaseModel):
+    """
+
+    :param item:
+    :return:
+    """
+    last_record_id = await database.execute(inventory.insert().values(**item.model_dump()))
+    return {**item.model_dump(), "id": last_record_id}
+
+
+@app.put(path="/{item_id}", response_model=InventoryItem)
+async def update_item(item_id: int, item: InventoryItemBaseModel):
+    """
+
+    :param item_id:
+    :param item:
+    :return:
+    """
+    await database.execute(inventory.update().where(inventory.c.id == item_id).values(**item.model_dump()))
+    return {**item.model_dump(), "id": item_id}
---
+++ src/sep/inventory/api/pmm.py
@@ -0,0 +1,155 @@
+"""
+PMM remote inventory
+"""
+from collections import (
+    namedtuple,
+    OrderedDict,
+)
+import json
+import netrc
+import ssl
+import urllib.parse
+import urllib.request
+
+
+def to_dict(self):
+    """
+
+    :param self:
+    :return:
+    """
+    if "services" in self._fields:
+        current_data = self._asdict()
+        current_data["services"] = []
+        for svc in self.services:
+            current_data["services"].append(svc.to_dict())
+        self = self._make(current_data.values())
+    return self._asdict()
+
+
+def to_json(self):
+    """
+
+    :param self:
+    :return:
+    """
+    return json.dumps(self.to_dict())
+
+
+Node = namedtuple("Node", ["id", "name", "type", "services"])
+Node.to_dict = to_dict
+Node.to_json = to_json
+
+Service = namedtuple("Service", ["cluster", "id", "name", "node_id", "type"])
+Service.to_dict = to_dict
+
+
+class InventorySource:
+    """PMM inventory source"""
+
+    __storage = {}
+
+    def __init__(self, uri: str, verify_tls: bool, **kwargs):
+        """Initialise the source
+
+        :param uri:
+        :param verify_tls:
+        :param kwargs:
+        :return:
+        """
+        self.base_uri = uri
+        self.uri = urllib.parse.urlparse(self.base_uri)
+
+        if not verify_tls:
+            # noinspection PyProtectedMember
+            tls_context = ssl._create_unverified_context()  # noqa: S323
+        else:
+            tls_context = ssl.create_default_context()
+        tls_handler = urllib.request.HTTPSHandler(context=tls_context)
+        handlers = [tls_handler]
+
+        if kwargs.get("use_cookies", []) and isinstance(kwargs["use_cookies"], list):
+            raise NotImplementedError("Cookie access is not currently supported")
+        else:
+            if "auth" not in kwargs:
+                credentials = netrc.netrc()  # TODO: add support for configurable paths
+                auth = credentials.authenticators(self.uri.hostname)
+            else:
+                auth = kwargs["auth"]
+
+            if not auth or len(auth) != 3:
+                raise UserWarning("No auth found")
+
+            password_mgr = urllib.request.HTTPPasswordMgrWithPriorAuth()
+            password_mgr.add_password(
+                realm=None, uri=self.base_uri, user=auth[0], passwd=auth[2], is_authenticated=True
+            )
+            handlers.append(urllib.request.HTTPBasicAuthHandler(password_mgr))
+
+        opener = urllib.request.build_opener(*handlers)
+        urllib.request.install_opener(opener)
+        self._headers = {"content-type": "application/json", "accept": "application/json"}
+
+    def request(self, uri: str, payload: bytes = b"", timeout: int = 60):
+        """Make a request"""
+        args = {} if not payload else dict(data=payload)
+        if timeout:
+            args["timeout"] = timeout
+        if not uri.startswith(("https:", "http:")):
+            raise ValueError(f"the URI does not appear to be HTTP: {uri}")
+        req = urllib.request.Request(uri, payload, self._headers, method="GET" if not payload else "POST")  # noqa: S310
+        with urllib.request.urlopen(req, **args) as resp:  # noqa: S310
+            data = resp.read().decode("utf-8")
+            return dict(raw=data, json=json.loads(data))
+
+    @property
+    def nodes(self) -> dict[Node]:
+        """Lookup all nodes"""
+        if "nodes" not in self.__storage:
+            req = self.request(f"{self.base_uri}/v1/inventory/Nodes/List", payload=b"{}")
+            nodes = {}
+            for node_type, data in req.get("json", {}).items():
+                # TODO: Lookup extra data for the node
+                for node in data:
+                    nodes[node["node_id"]] = Node(
+                        id=node["node_id"],
+                        name=node["node_name"],
+                        services=[v for _, v in self.services.items() if v.node_id == node["node_id"]],
+                        type=node_type,
+                    )
+            self.__storage["nodes"] = nodes
+        return self.__storage["nodes"]
+
+    @property
+    def organisations(self) -> list[OrderedDict]:
+        """
+        Lookup the organisations
+
+        :return:
+        """
+        if "orgs" not in self.__storage:
+            self.__storage["orgs"] = OrderedDict()
+            resp = self.request(f"{self.base_uri}/graph/api/orgs", payload=b"")
+            for org in resp.get("json", []):
+                if "name" not in org:
+                    continue
+                self.__storage["orgs"][org["name"]] = org
+        return self.__storage["orgs"]
+
+    @property
+    def services(self) -> dict[Service]:
+        """Lookup all services"""
+        if "services" not in self.__storage:
+            req = self.request(f"{self.base_uri}/v1/inventory/Services/List", payload=b"{}")
+            services = {}
+            for service_type, data in req.get("json", {}).items():
+                for service in data:
+                    services[service["service_id"]] = Service(
+                        cluster=service.get("cluster"),
+                        id=service["service_id"],
+                        name=service["service_name"],
+                        node_id=service["node_id"],
+                        type=service_type,
+                    )
+            self.__storage["services"] = services
+        return self.__storage["services"]
