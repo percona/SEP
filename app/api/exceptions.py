f58574681ca00fbd95f1597f41a291d8fa6cd2cf
Yan Orestes <yan.orestes@percona.com>
2024-09-18 18:11:27 -0300

    Create HTTPNotFoundException

--- app/api/exceptions.py
+++ app/api/exceptions.py
@@ -1,5 +1,24 @@
 """Define reusable API exceptions."""
 
+from fastapi import HTTPException
+from fastapi import status
+
 from app.core.auth.exceptions import HTTPForbiddenException
+
+
+class HTTPNotFoundException(HTTPException):
+    """Exception raised for resource not found (HTTP 404).
+
+    Parameters
+    ----------
+    detail : str, optional
+        A message providing additional details about the exception. Defaults to
+        "Not Found".
+
+    """
+
+    def __init__(self, detail: str = "Not Found"):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
 
 InactiveUserException = HTTPForbiddenException("User is not active")
