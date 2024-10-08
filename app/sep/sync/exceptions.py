7a3b245a0c2f404a325f44035d7c579b0fb0303b
Yan Orestes <yan.orestes@percona.com>
2024-10-08 09:55:26 -0300

    Create SyncFailError

--- app/sep/sync/exceptions.py
+++ app/sep/sync/exceptions.py
@@ -1,5 +1,6 @@
-"""Define reusable Sync exceptions."""
+"""Define reusable Sync exceptions for the SEP app."""
 
+from app.sep.models import SyncInventoryEntityTypeEnum
 from app.sep.models import SyncItem
 
 
@@ -10,6 +11,32 @@ class SyncError(Exception):
     application. It serves as a common ancestor for more specific synchronization error
     types.
     """
+
+
+class SyncFailError(SyncError):
+    """Raise when a synchronization process fails.
+
+    This exception is triggered when an error occurs during the synchronization of a
+    specific entity, resulting in the associated `SyncItem` being marked as failed.
+
+    Attributes
+    ----------
+    entity_type : SyncInventoryEntityTypeEnum
+        The type of the entity that failed to synchronize.
+    sync_item : SyncItem
+        The `SyncItem` instance that failed during synchronization.
+
+    """
+
+    def __init__(
+        self,
+        entity_type: SyncInventoryEntityTypeEnum,
+        sync_item: SyncItem,
+    ) -> None:
+        self.entity_type = entity_type
+        self.sync_item = sync_item
+        message = f"Failed to sync {entity_type}: {sync_item}"
+        super().__init__(message)
 
 
 class SyncItemAlreadyInProgressError(SyncError):
