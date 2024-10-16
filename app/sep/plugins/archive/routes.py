970b7a700f3f9bc0e67c6bd27004c4a15217501a
peter-o-addo <peter.addo@percona.com>
2024-10-16 11:39:37 +0300

    Add archive plugin and update template as jinja2

---
+++ app/sep/plugins/archive/routes.py
@@ -0,0 +1,65 @@
+import logging
+
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+
+from app.sep.config import sep_settings
+from app.sep.deps import DefaultContext
+from app.sep.deps import InventoryAPI
+from app.sep.deps import TaskAPI
+from app.sep.deps import IsAuthenticated
+from app.tasks.models import TaskHistoryStatusEnum
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+templates = sep_settings.TEMPLATES
+
+@router.get("/", dependencies=[IsAuthenticated], response_class=HTMLResponse)
+async def alters_index(
+    request: Request,
+    context: DefaultContext,
+    tasks_api: TaskAPI,
+    inventory_api: InventoryAPI,
+) -> HTMLResponse:
+    """Homepage of alters plugin."""
+    all_hosts = await inventory_api.get("/")
+    tasks = []
+    for task in await tasks_api.get("/"):
+        if task.get("owner") == "alters":  # TODO: filter on query
+            data = task["data"]
+            meta = data["TaskGroups"][0]["Tasks"][0]["Meta"]
+            taskinfo = {
+                "hostname": data["Constraints"][0]["RTarget"],
+                "name": task["name"],
+                "table": f'{meta["schema_name"]}.{meta["table_name"]}',
+                "id": task["id"],
+            }
+            tasks.append(taskinfo)
+    history_tasks = []
+    scheduled_tasks = []
+    running_tasks = []
+    for task in tasks:
+        history = await tasks_api.get(f"/history/{task['name']}")
+        for hist in history:
+            match TaskHistoryStatusEnum(hist["status"]):
+                case TaskHistoryStatusEnum.SUCCESS | TaskHistoryStatusEnum.FAILED:
+                    history_tasks.append(hist)
+                case TaskHistoryStatusEnum.PENDING:
+                    scheduled_tasks.append(hist)
+                case TaskHistoryStatusEnum.RUNNING:
+                    running_tasks.append(hist)
+    context.update(
+        {
+            "hosts": all_hosts,
+            "archives": tasks,
+            "scheduled_tasks": scheduled_tasks,
+            "running_tasks": running_tasks,
+            "history_tasks": history_tasks,
+        },
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name="archiver/index.html",
+        context=context,
+    )
