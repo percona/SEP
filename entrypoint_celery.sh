d2be613a66c1a605759856877101baf4d8deb9f3
yyyyyyy <yan.orestes@percona.com>
2024-11-27 16:55:09 -0300

    SEP-202: Fix periodic tasks bug with dockerized app (#68)

--- entrypoint_celery.sh
+++ entrypoint_celery.sh
@@ -7,4 +7,11 @@ while ! nc -z redis 6379; do
 done
 echo "Redis started"
 
+# Wait for PostgreSQL to be available
+echo "Waiting for postgres..."
+while ! nc -z sep-db 5432; do
+  sleep 1
+done
+echo "PostgreSQL started"
+
 celery -A app.tasks.celery "$@"
