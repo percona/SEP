4c513707af13cf9369f7a8a7ef97f554d0265e50
peter-o-addo <peter.addo@percona.com>
2024-12-02 13:21:36 +0200

    Add test for CSRF protection

---
+++ app/tests/sep/csrf/test_csrf_protect.py
@@ -0,0 +1,105 @@
+"""Define tests for the CSRF protection for app.sep module."""
+
+from http import HTTPStatus
+
+import pytest
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.testclient import TestClient
+from fastapi_csrf_protect import CsrfProtect
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+
+from app.sep.config import CsrfSettings
+from app.sep.deps import IsCsrfValidated
+from app.sep.middleware import CSRFMiddleware
+
+
+@pytest.fixture
+def test_client() -> TestClient:
+    """Provides a TestClient with CSRF protection and routes configured."""
+    app = FastAPI()
+
+    @app.get("/gen-token", response_class=JSONResponse)
+    def generate(request: Request):
+        response: JSONResponse = JSONResponse(
+            status_code=200,
+            content={"detail": "OK", "csrf_token": request.state.csrf_token},
+        )
+        return response
+
+    @app.post("/protected", dependencies=[IsCsrfValidated], response_class=JSONResponse)
+    def protected():
+        response: JSONResponse = JSONResponse(status_code=200, content={"detail": "OK"})
+        return response
+
+    app.add_middleware(CSRFMiddleware)
+
+    @app.exception_handler(CsrfProtectError)
+    def csrf_protect_error_handler(request: Request, exc: CsrfProtectError):
+        return JSONResponse(
+            status_code=exc.status_code, content={"detail": exc.message}
+        )
+
+    return TestClient(app)
+
+
+def test_valid_csrf_token(test_client: TestClient):
+    """Test the CSRF token generation and validation process."""
+
+    @CsrfProtect.load_config
+    def get_configs():
+        return CsrfSettings()
+
+    response = test_client.get("/gen-token")
+    assert response.status_code == HTTPStatus.OK
+
+    csrf_token = response.json().get("csrf_token", None)
+    assert csrf_token is not None
+
+    csrf_cookie = test_client.cookies.get("fastapi-csrf-token", None)
+    assert csrf_cookie is not None
+
+    response = test_client.post(
+        "/protected",
+        data={"csrf-token": csrf_token},
+        cookies={"fastapi-csrf-token": csrf_cookie},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"detail": "OK"}
+
+
+def test_invalid_csrf_token(test_client: TestClient):
+    """Test handling of invalid CSRF tokens and missing cookies."""
+
+    @CsrfProtect.load_config
+    def get_configs():
+        return CsrfSettings()
+
+    response = test_client.get("/gen-token")
+    assert response.status_code == HTTPStatus.OK
+
+    csrf_token = response.json().get("csrf_token", None)
+    assert csrf_token is not None
+
+    csrf_cookie = test_client.cookies.get("fastapi-csrf-token", None)
+    assert csrf_cookie is not None
+
+    response = test_client.post("/protected")
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Missing Cookie: `fastapi-csrf-token`."}
+
+    response = test_client.post(
+        "/protected",
+        data={"csrf-token": "invalid token"},
+        cookies={"fastapi-csrf-token": csrf_cookie},
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "The CSRF signatures submitted do not match."}
+
+    response = test_client.post(
+        "/protected",
+        data={"csrf-token": csrf_token},
+        cookies={"fastapi-csrf-token": "invalid cookie"},
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {"detail": "The CSRF token is invalid."}
