510bc7cd6f94aaeb7861088e1936e0773f904deb
Yan Orestes <yan.orestes@percona.com>
2024-12-20 16:31:14 -0300

    Form adjustments

--- app/sep/plugins/backup/models.py
+++ app/sep/plugins/backup/models.py
@@ -50,48 +50,47 @@ class BackupConfigAll(BaseCaseInsensitiveModel):
     compress: bool = True
     check_disk_space: bool = True
     encrypt: bool = False
-    encrypt_using_tmpdir: bool | EmptyStrToNone = None
+    encrypt_using_tmpdir: bool = False
     only_if_running_slave: bool = False
     only_if_read_only: bool = False
-    logging_dir: str | EmptyStrToNone = None
-    backup_dir: str | EmptyStrToNone = None
-    defaults_file: str | EmptyStrToNone = None
+    logging_dir: RequiredStr | EmptyStrToNone = None
+    backup_dir: RequiredStr | EmptyStrToNone = None
+    defaults_file: RequiredStr | EmptyStrToNone = None
     mydumper_daily_purge: int | EmptyStrToNone = None
     mydumper_weekly_purge: int | EmptyStrToNone = None
-    mydumper_schemas: bool | EmptyStrToNone = None
-    mydumper_dump_triggers: bool | EmptyStrToNone = None
-    mydumper_desync_pxc: bool | EmptyStrToNone = None
-    mydumper_less_locking: bool | EmptyStrToNone = None
-    mydumper_use_numa: bool | EmptyStrToNone = None
+    mydumper_dump_triggers: bool = False
+    mydumper_desync_pxc: bool = False
+    mydumper_less_locking: bool = False
+    mydumper_use_numa: bool = False
     mydumper_extra_args: str | EmptyStrToNone = None
-    use_ftwrl_guardian: bool | EmptyStrToNone = None
+    use_ftwrl_guardian: bool = False
     xtrabackup_copies: int | EmptyStrToNone = None
-    xtrabackup_kill_queries: bool | EmptyStrToNone = None
+    xtrabackup_kill_queries: bool = False
     xtrabackup_kill_queries_timeout: int | EmptyStrToNone = None
     xtrabackup_kill_query_type: Literal["select", "all"] | EmptyStrToNone = None
-    xtrabackup_verify: bool | EmptyStrToNone = None
-    xtrabackup_prepare: bool | EmptyStrToNone = None
-    xtrabackup_prepare_memory: str | EmptyStrToNone = None
-    xtrabackup_desync_pxc: bool | EmptyStrToNone = None
-    xtrabackup_rsync: bool | EmptyStrToNone = None
-    xtrabackup_slave_info: bool | EmptyStrToNone = None
-    xtrabackup_defaults_file: str | EmptyStrToNone = None
-    xtrabackup_extra_args: str | EmptyStrToNone = None
+    xtrabackup_verify: bool = False
+    xtrabackup_prepare: bool = False
+    xtrabackup_prepare_memory: RequiredStr | EmptyStrToNone = None
+    xtrabackup_desync_pxc: bool = False
+    xtrabackup_rsync: bool = False
+    xtrabackup_slave_info: bool = False
+    xtrabackup_defaults_file: RequiredStr | EmptyStrToNone = None
+    xtrabackup_extra_args: RequiredStr | EmptyStrToNone = None
     xtrabackup_incremental_method: (
         Literal["less_space", "fast_restore"] | EmptyStrToNone
     ) = None
     xtrabackup_incremental_cycle: Literal["daily", "weekly"] | EmptyStrToNone = None
-    xtrabackup_local_ssh_destination: str | EmptyStrToNone = None
-    xtrabackup_aes256_keyfile: str | EmptyStrToNone = None
-    xtrabackup_stop_slave: bool | EmptyStrToNone = None
-    xtrabackup_lock_ddl: bool | EmptyStrToNone = None
+    xtrabackup_local_ssh_destination: RequiredStr | EmptyStrToNone = None
+    xtrabackup_aes256_keyfile: RequiredStr | EmptyStrToNone = None
+    xtrabackup_stop_slave: bool = False
+    xtrabackup_lock_ddl: bool = False
     xtrabackup_bin_cmd: (
         Literal["xtrabackup", "mariadb-backup", "innobackupex"] | EmptyStrToNone
     ) = None
-    s3_bucket: str | EmptyStrToNone = None
-    s3_storage_class: str | EmptyStrToNone = None
-    skip_s3_safety_check: bool | EmptyStrToNone = None
-    rsync_path: str | EmptyStrToNone = None
+    s3_bucket: RequiredStr | EmptyStrToNone = None
+    s3_storage_class: RequiredStr | EmptyStrToNone = None
+    skip_s3_safety_check: bool = False
+    rsync_path: RequiredStr | EmptyStrToNone = None
 
 
 class BackupCreate(BackupConfigAll):
