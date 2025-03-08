6e090deb05d1dadd56e0eb93766ebd83f6681855
Yan Orestes <yan.orestes@percona.com>
2025-03-08 19:59:14 -0300

    Add create_new_engine parameter to get_async_session_maker

--- app/sep/db.py
+++ app/sep/db.py
@@ -1,20 +1,29 @@
 """Define database initialization and utility functions for SEP."""
 
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncEngine, create_async_engine
 
 from app.core.db.utils import get_async_session_maker_from_engine
 from app.core.utils import json_serializer
 from app.sep.config import sep_settings
 
-engine = create_async_engine(
-    sep_settings.DATABASE.URL,
-    echo=False,
-    json_serializer=json_serializer,
-)
+
+def get_async_engine() -> AsyncEngine:
+    """Create and return SQLAlchemy AsyncEngine for the SEP database.
+
+    :return: The SQLAlchemy AsyncEngine for the SEP database.
+    :rtype: AsyncEngine
+    """
+    return create_async_engine(
+        sep_settings.DATABASE.URL,
+        echo=False,
+        json_serializer=json_serializer,
+    )
 
 
-def get_async_session_maker() -> sessionmaker:
+engine = get_async_engine()
+
+
+def get_async_session_maker(*, create_new_engine: bool = False) -> async_sessionmaker:
     """Return a new asynchronous session maker for database operations.
 
     This function creates a new SQLAlchemy asynchronous session maker using the
@@ -23,4 +32,6 @@ def get_async_session_maker() -> sessionmaker:
     :return: A new asynchronous session maker.
     :rtype: sessionmaker
     """
+    if create_new_engine:
+        return get_async_session_maker_from_engine(get_async_engine())
     return get_async_session_maker_from_engine(engine)
