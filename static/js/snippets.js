c540453d7b93253c6c3d5edbdaa6f2c0a1650d08
Yan Orestes <yan.orestes@percona.com>
2025-03-11 15:08:57 -0300

    Only allow approval after snippet inspection

--- static/js/snippets.js
+++ static/js/snippets.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
     $('.downloadSnippetForm').submit(function(e) {
         e.preventDefault();
-        const anchorTag = $(this).siblings("a.downloadSnippet")[0];
+        const anchorTag = $(this).siblings('a.downloadSnippet')[0];
         anchorTag.click();
     });
+
+    $('a.downloadSnippet').click(function() {
+        const $anchor = $(this);
+        const snippetFilename = $.escapeSelector($anchor.data('snippet-filename'));
+        const $approveButton = $(`#${snippetFilename}-approval > button[type=submit]:is([disabled])`);
+        $approveButton.prop("disabled", false);
+        $approveButton.attr("title", $approveButton.data("next-title"));
+    })
 });
