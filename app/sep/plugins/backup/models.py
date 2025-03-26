71da4e269b6fda552b421f484608696c0ff8ad1e
peter-o-addo <peter.addo@percona.com>
2025-03-26 10:46:09 +0200

    add binlog config section on backup form

--- app/sep/plugins/backup/models.py
+++ app/sep/plugins/backup/models.py
@@ -22,6 +22,7 @@ class BackupType(EnumFieldMixin, StrEnum):
 
     MYDUMPER = "M"
     XTRABACKUP = "X"
+    BINLOG = "B"
 
 
 class UploadProvider(EnumFieldMixin, StrEnum):
@@ -87,6 +88,11 @@ class BackupConfigAll(BaseCaseInsensitiveModel):
     xtrabackup_bin_cmd: (
         Literal["xtrabackup", "mariadb-backup", "innobackupex"] | EmptyStrToNone
     ) = None
+    binlog_prefix: RequiredStr | EmptyStrToNone = None
+    binlog_purge_days: int | EmptyStrToNone = None
+    binlog_extra_args: RequiredStr | EmptyStrToNone = None
+    binlog_compress_cmd: RequiredStr | EmptyStrToNone = None
+    binlog_cmd: RequiredStr | EmptyStrToNone = None
     s3_bucket: RequiredStr | EmptyStrToNone = None
     s3_storage_class: RequiredStr | EmptyStrToNone = None
     skip_s3_safety_check: bool = False
