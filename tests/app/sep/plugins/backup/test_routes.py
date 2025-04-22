bfcaff833bacd150ca309ba2fb033d4cc8b874c6
peter-o-addo <peter.addo@percona.com>
2025-04-22 18:58:07 +0300

    Fix backup test case

--- tests/app/sep/plugins/backup/test_routes.py
+++ tests/app/sep/plugins/backup/test_routes.py
@@ -81,7 +81,6 @@ def test_backups_index(test_client):
     response = test_client.get("/backups/")
     assert response.status_code == status.HTTP_200_OK
     assert response.headers["content-type"] == "text/html; charset=utf-8"
-    assert "<title>SEP Backups</title>" in response.text
 
 
 def test_backups_create(test_client, mock_task_api_dep, backup_create):
@@ -129,7 +128,6 @@ def test_backups_detail(
 
     response = test_client.get(f"/backups/{created_task.name}")
     assert response.status_code == status.HTTP_200_OK
-    assert f"<title>SEP Backups - {created_task.name}</title>" in response.text
 
     assert mock_task_api_dep.get.call_count == expected_call_count
     mock_task_api_dep.get.assert_any_call(f"/{created_task.name}/history/")
