154a76db27e9dca2a24a4c4dfd5e00abf68a7d70
peter-o-addo <peter.addo@percona.com>
2025-06-05 23:03:56 +0300

    Fix audit issue

--- app/tasks/routes.py
+++ app/tasks/routes.py
@@ -322,32 +322,34 @@ async def stream_task_history_logs(
         raise HTTPConflictException("Task history is pending.")
     if task_history.status == TaskHistoryStatusEnum.RUNNING:
         stream_logs_generator = (
-            f"{
+            (
                 log_line.model_copy(
                     update={
-                        'msg': presidio_anonymize_log(
+                        "msg": presidio_anonymize_log(
                             log_line.msg, task_history.task.anonymize
                         )
-                        if log_line.step in ('run-script', 'step1')
+                        if log_line.step in ("run-script", "step1")
                         else log_line.msg
                     }
                 ).model_dump_json()
-            }\n"
+                + "\n"
+            )
             async for log_line in executor.stream_logs(task_history)
         )
     else:
         stream_logs_generator = (
-            f"{
+            (
                 TaskLog(
                     step=step,
                     type=log_type,
                     msg=presidio_anonymize_log(
                         log[log_type], task_history.task.anonymize
                     )
-                    if step in ('run-script', 'step1')
+                    if step in ("run-script", "step1")
                     else log[log_type],
                 ).model_dump_json()
-            }\n"
+                + "\n"
+            )
             for step, log in task_history.execution_request.tracking.get(
                 "task_logs", {}
             ).items()
