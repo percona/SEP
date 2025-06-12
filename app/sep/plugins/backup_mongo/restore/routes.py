37526299ef22c78282044e024ebca0bf27523b8c
arlindo-neto <arlindo.neto@percona.com>
2025-06-12 13:29:33 -0300

    added models, controller and routes to Restore

---
+++ app/sep/plugins/backup_mongo/restore/routes.py
@@ -0,0 +1,130 @@
+"""Define routes for the restores plugin."""
+
+import logging
+from typing import Annotated, Any
+
+import yaml
+from fastapi import APIRouter, Depends, Form, Request, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import FutureDatetime
+
+from app.sep.config import sep_settings
+from app.sep.deps import DefaultContext, IsAuthenticated, IsCsrfValidated, TaskAPI
+from app.sep.plugins.backup_mongo.models import BackupType
+from app.sep.plugins.backup_mongo.restore.deps import (
+    get_restores_index_context,
+    RestoreGeneratedTask,
+    RestoresTask,
+)
+from app.tasks.models import TaskHistoryStatusEnum
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+templates = sep_settings.TEMPLATES
+
+
+@router.get("/", dependencies=[IsAuthenticated], response_class=HTMLResponse)
+async def restores_index(
+    request: Request,
+    context: Annotated[dict[str, Any], Depends(get_restores_index_context)],
+) -> HTMLResponse:
+    """Homepage of restores plugin."""
+    return templates.TemplateResponse(
+        request=request,
+        name="backups/restore/index.html",
+        context=context,
+    )
+
+
+@router.post(
+    "/", dependencies=[IsAuthenticated, IsCsrfValidated], response_class=HTMLResponse
+)
+async def restores_create(
+    request: Request,
+    task: RestoreGeneratedTask,
+    task_api: TaskAPI,
+) -> RedirectResponse:
+    """Create new restores task."""
+    logger.debug("Create restores task: %s", task)
+    await task_api.post(
+        "/",
+        json=task.model_dump(),
+    )
+    task_path = request.url_for("restores_detail", task_name=task.name)
+    return RedirectResponse(
+        task_path,
+        status_code=status.HTTP_303_SEE_OTHER,
+    )  # TODO: Custom redirect class  # noqa: TD002, TD003
+
+
+@router.get("/{task_name}", dependencies=[IsAuthenticated], response_class=HTMLResponse)
+async def restores_detail(
+    task: RestoresTask,
+    request: Request,
+    context: DefaultContext,
+    tasks_api: TaskAPI,
+) -> HTMLResponse:
+    """Retrieve restores task."""
+    data = task.data
+    meta = data["meta"]
+    task_config = yaml.safe_load(meta["config"])
+    server_config = task_config["SERVER_LIST"][0]
+    task_data = {
+        "name": task.name,
+        "created_at": task.created_at,
+        "updated_at": task.updated_at,
+        "hostname": meta["target"],
+        "meta": meta,
+        "host": server_config["DEST_HOST"],
+        "port": server_config["DEST_PORT"],
+        "database": server_config.get("DATABASER"),
+        "restore_type": BackupType(server_config["BACKUP_TYPE"]).name,
+        "delete_url": request.url_for("restores_delete", task_name=task.name),
+    }
+    context["task"] = task_data
+    context["history"] = await tasks_api.get(f"/{task.name}/history/")
+    context["running_tasks"] = await tasks_api.get(
+        f"/{task.name}/history/", params={"status": TaskHistoryStatusEnum.RUNNING}
+    )
+    context["stats"] = await tasks_api.get(f"/stats/{task.name}")
+    return templates.TemplateResponse(
+        request=request,
+        name="backups/restore/details.html",
+        context=context,
+    )
+
+
+@router.post(
+    "/{task_name}",
+    dependencies=[IsAuthenticated, IsCsrfValidated],
+    response_class=RedirectResponse,
+)
+async def restores_execute(
+    request: Request,
+    task: RestoresTask,
+    tasks_api: TaskAPI,
+    eta: Annotated[FutureDatetime | None, Form()] = None,
+) -> RedirectResponse:
+    """Execute restores task."""
+    await tasks_api.post(
+        f"/execute/{task.name}",
+        json={"eta": eta},
+    )  # TODO: send meta form fields  # noqa: TD002, TD003
+    task_path = request.url_for("restores_detail", task_name=task.name)
+    return RedirectResponse(task_path, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@router.post(
+    "/{task_name}/delete",
+    dependencies=[IsAuthenticated, IsCsrfValidated],
+    response_class=RedirectResponse,
+)
+async def restores_delete(
+    request: Request,
+    task: RestoresTask,
+    tasks_api: TaskAPI,
+) -> RedirectResponse:
+    """Delete restores task."""
+    await tasks_api.delete(f"/{task.name}")
+    task_path = request.url_for("restores_index")
+    return RedirectResponse(task_path, status_code=status.HTTP_303_SEE_OTHER)
