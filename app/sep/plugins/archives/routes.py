7ad168751ad3b86a999ccfb24c792b61ad01fd65
peter-o-addo <peter.addo@percona.com>
2025-06-26 17:58:06 +0300

    Add edit modal at archiver

--- app/sep/plugins/archives/routes.py
+++ app/sep/plugins/archives/routes.py
@@ -4,23 +4,27 @@ import logging
 from typing import Annotated, Any
 
 import yaml
-from fastapi import APIRouter, Depends, Form, Request, status
+from fastapi import APIRouter, Depends, Form, HTTPException, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from pydantic import FutureDatetime
 
+from app.inventory.models import ServiceTypeEnum
 from app.sep.config import sep_settings
 from app.sep.deps import (
     DefaultContext,
     HasNoConflictedRunningTasks,
+    InventoryAPI,
     IsAuthenticated,
     IsCsrfValidated,
     TaskAPI,
 )
+from app.sep.middleware import messages
 from app.sep.plugins.archives.deps import (
     ArchivesGeneratedTask,
     ArchivesTask,
     get_archives_index_context,
 )
+from app.sep.plugins.archives.models import PurgeConfigItem
 from app.tasks.models import TaskHistoryStatusEnum
 
 logger = logging.getLogger(__name__)
@@ -67,12 +71,14 @@ async def archives_detail(
     task: ArchivesTask,
     request: Request,
     context: DefaultContext,
+    inventory_api: InventoryAPI,
     tasks_api: TaskAPI,
 ) -> HTMLResponse:
     """Retrieve archives task."""
     data = task.data
     meta = data["meta"]
     task_config = yaml.safe_load(meta["config"])
+    all_server = task_config["ALL"]
     purge_item = task_config["PURGE_LIST"][0]
     task_data = {
         "name": task.name,
@@ -97,12 +103,35 @@ async def archives_detail(
         task_data["source_query"] = source_query
     if dest_file:
         task_data["dest_file"] = dest_file
-    context["task"] = task_data
+    task_data["source_host"] = all_server.get("SOURCE_HOST")
+    task_data["source_port"] = all_server.get("SOURCE_PORT")
+    context["task"] = {
+        **task_data,
+        **PurgeConfigItem.model_validate(purge_item).model_dump(),
+    }
     context["history"] = await tasks_api.get(f"/{task.name}/history/")
     context["running_tasks"] = await tasks_api.get(
         f"/{task.name}/history/", params={"status": TaskHistoryStatusEnum.RUNNING}
     )
     context["stats"] = await tasks_api.get(f"/stats/{task.name}")
+
+    try:
+        executor_hosts = await tasks_api.get("/hosts/")
+    except HTTPException as exc:
+        executor_hosts = {}
+        messages.error(request, exc.detail)
+
+    services = await inventory_api.get(
+        "/services/", params={"service_type": ServiceTypeEnum.MYSQL}
+    )
+    for service in services:
+        service["schemas"] = await inventory_api.get(
+            f"/services/{service['id']}/schemas/",
+        )
+    context["services"] = services
+
+    context["executor_hosts"] = set(executor_hosts.values()) | {task_data["hostname"]}
+
     return templates.TemplateResponse(
         request=request,
         name="archiver/details.html",
