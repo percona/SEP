117f705592b9584c4cd8f38abc4fa968e9dcd33c
Max Bubenick <max.bubenick@percona.com>
2025-08-19 15:29:32 -0300

    lint app/sep/plugins/alters/routes.py

--- app/sep/plugins/alters/routes.py
+++ app/sep/plugins/alters/routes.py
@@ -148,9 +148,7 @@ async def alters_detail(
 
     # If the task has a parent, redirect to the parent task detail page
     if data.get("parent"):
-        task_path = request.url_for(
-            "alters_detail", task_name=data["parent"]
-        )
+        task_path = request.url_for("alters_detail", task_name=data["parent"])
         return RedirectResponse(task_path, status_code=status.HTTP_303_SEE_OTHER)
 
     context["task"] = task_data
