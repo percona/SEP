6686c9624770297ba1bd69146e2e2a1acf36be3e
Max Bubenick <max.bubenick@percona.com>
2025-09-07 16:06:20 -0300

    fix

--- app/sep/plugins/backup_mongo/routes.py
+++ app/sep/plugins/backup_mongo/routes.py
@@ -22,14 +22,10 @@ from app.sep.plugins.backup_mongo.deps import (
 )
 from app.tasks.models import TaskHistoryStatusEnum
 
-from .restore.routes import router as restore_router
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
 templates = sep_settings.TEMPLATES
-
-
-router.include_router(restore_router, prefix="/restores", tags=["restores"])
 
 
 @router.get("/", dependencies=[IsAuthenticated], response_class=HTMLResponse)
