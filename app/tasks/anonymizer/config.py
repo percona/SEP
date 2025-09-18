1acb7e3752ace82af57f484a4f4e29ecab3681e0
Yan Orestes <yan.orestes@percona.com>
2025-09-17 22:39:16 -0300

    Use en_core_web_sm

--- app/tasks/anonymizer/config.py
+++ app/tasks/anonymizer/config.py
@@ -37,7 +37,7 @@ class AnonymizerSettings(BaseYamlSettings):
         owners. Defaults to an empty set.
     :type DEFAULT_ENTITIES: defaultdict[TaskOwner, set[PIIEntity]]
     :param NLP_MODELS: A mapping of language codes to spaCy model names for NLP
-        processing. Defaults to `{"en": "en_core_web_md"}`.
+        processing. Defaults to `{"en": "en_core_web_sm"}`.
     :type NLP_MODELS: dict[str, str]
     """
 
@@ -45,7 +45,7 @@ class AnonymizerSettings(BaseYamlSettings):
     DEFAULT_ENTITIES: defaultdict[TaskOwner, set[PIIEntity]] = Field(
         default_factory=dict, validate_default=True
     )
-    NLP_MODELS: dict[str, str] = Field({"en": "en_core_web_md"}, min_length=1)
+    NLP_MODELS: dict[str, str] = Field({"en": "en_core_web_sm"}, min_length=1)
 
     @field_validator("DEFAULT_ENTITIES", mode="before")
     @classmethod
