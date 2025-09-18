1c2b2784b10edfb670127193cdcce01567281de4
yyyyyyy <yan.orestes@percona.com>
2025-09-18 14:06:28 -0300

    SEP-589: Make spacy models configurable and use en_core_web_sm as default (#249)

--- app/sep/plugins/backup/routes.py
+++ app/sep/plugins/backup/routes.py
@@ -25,7 +25,7 @@ from app.sep.plugins.backup.deps import (
     parse_backup_task_data,
 )
 from app.sep.plugins.backup.models import BackupType
-from app.tasks.anonymizer import PIIEntity
+from app.tasks.anonymizer.entities import PIIEntity
 from app.tasks.models import TaskHistoryStatusEnum
 
 from .restore.routes import router as restore_router
