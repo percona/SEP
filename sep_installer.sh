581e5f0bdd7bbc3d3ee76e89cafaeea691a2203f
cezmunsta <github@incoming-email.co.uk>
2025-09-30 13:08:58 +0100

    Added example steps for Ubuntu 24.04 as comment

--- sep_installer.sh
+++ sep_installer.sh
@@ -194,7 +194,15 @@ check_prereqs() {
 	# sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 	### Example for Ubuntu 24.04
-	# sudo apt install docker docker-compose-v2
+	# sudo apt-get update
+	# sudo apt-get install ca-certificates curl
+	# sudo install -m 0755 -d /etc/apt/keyrings
+	# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	# sudo chmod a+r /etc/apt/keyrings/docker.asc
+	# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	# sudo apt-get update
+	# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 	test "${check_prereqs_out}" = "" || {
 		printf "ERROR: the following commands are unavailable - %s\n" "${check_prereqs_out}";
