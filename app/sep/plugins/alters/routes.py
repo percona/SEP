188af81f5ff4ae2476ed3c8ca056f67919725ec3
Peter Addo <peter.addo@percona.com>
2025-10-08 17:03:08 +0300

    SEP-571: Add created_by, last_edit_by, and executed_by at Alter plugin (#239)

--- app/sep/plugins/alters/routes.py
+++ app/sep/plugins/alters/routes.py
@@ -160,6 +160,8 @@ async def alters_detail(
         "name": task.name,
         "created_at": task.created_at,
         "updated_at": task.updated_at,
+        "created_by": task.created_by,
+        "last_updated_by": task.last_updated_by,
         "hostname": meta["target"],
         "table": f"{meta['_schema_name']}.{meta['_table_name']}",
         "cmd": f"{meta['command']} {meta['args']}",
