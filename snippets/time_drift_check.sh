17ca0960834a16a1ddfd5622396017ad2ac07267
Duy Nguyen <crimethinking@users.noreply.github.com>
2026-02-25 15:40:13 +0700

    revise snippet

--- snippets/time_drift_check.sh
+++ snippets/time_drift_check.sh
@@ -7,24 +7,23 @@
 # sudo: optional
 # ---
 
-# Check if timedatectl is reporting unsynchronized system clock
-# and check for PMM admin time drift status 
-timedatectl | grep --color=never -B1 'System clock synchronized: no'
-pmm-admin status | grep --color=never 'Time drift'
+set -euo pipefail
 
-# Check for running NTP implementation on system in order to proceed
-if command -v systemctl &> /dev/null; then
-    if systemctl is-active -q ntpd; then
-        echo "ntpd is running"
-        exit 0
-    elif systemctl is-active -q chronyd; then
-        echo "chronyd is running"
-        exit 0
-    elif systemctl is-active -q systemd-timesyncd; then
-        echo "systemd-timesyncd is running"
-        exit 0
-    fi
+# Check if timedatectl is reporting unsynchronized system clock and check for PMM admin time drift status
+echo "System NTP synchronization status: $(timedatectl show -p NTPSynchronized --value)"
+echo "PMM time drift: $(pmm-admin status --json | jq -M '.pmm_agent_status.server_clock_drift / 1000' )" "μs"
+
+# Report back on what NTP implementation the system is running
+if systemctl is-active -q ntpd; then
+  echo "ntpd is running."
+  exit 0
+elif systemctl is-active -q chronyd; then
+  echo "chronyd is running."
+  exit 0
+elif systemctl is-active -q systemd-timesyncd; then
+  echo "systemd-timesyncd is running."
+  exit 0
+else
+  echo "Could not determine running NTP implementation or no NTP service is active."
+  exit 1
 fi
-
-echo "Could not determine running NTP implementation or no NTP service is active."
-exit 1
