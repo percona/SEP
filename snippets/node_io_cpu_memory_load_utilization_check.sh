ccbef2006cfd980ddc92a071c5e8ece024aaeeff
Duy Nguyen <crimethinking@users.noreply.github.com>
2026-02-25 23:06:21 +0700

    fix lints

--- snippets/node_io_cpu_memory_load_utilization_check.sh
+++ snippets/node_io_cpu_memory_load_utilization_check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # ---
-# title: "Node IO/CPU/Load Utilization Check"
-# description: "This script outputs I/O, CPU, and load utilization of system to assist in troubleshooting high system load scenarios."
+# title: "Node IO/CPU/Load/Memory Utilization Check"
+# description: "This script outputs I/O, CPU, load, and memory utilization of system to assist in troubleshooting high system load scenarios."
 # allow_extra_args: false
 # sudo: always
 # ---
@@ -16,24 +16,26 @@ section() {
 
 # Display CPU/load utilization
 section "CPU/load utilization"
-
 uptime
 top -b -n10 | grep --color=never "load average" -A50
 
 # Display I/O utilization
-section "I/O Utilization"
-
-if command -v iotop &>/dev/null; then
+section "I/O utilization"
+if command -v iotop &> /dev/null; then
     iotop -o -b -n10
 else
     echo "iotop not found, please install to view I/O utilization."
 fi
 
-# Display virtual memory statistics
+# Display memory statistics
+section "Memory statistics"
 section "Virtual memory statistics"
-
-if command -v vmstat &>/dev/null; then
+if command -v vmstat &> /dev/null; then
     vmstat -t -n 1 10
 else
     echo "vmstat not found, please install to view virtual memory statistics."
 fi
+section "Top memory-consuming processes (ps)"
+ps aux --sort=-%mem | head -n 20
+section "Top memory-consuming processes (top)"
+top -b -n1 -o %MEM | head -n 40
