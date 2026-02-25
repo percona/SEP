58652c1cb1bb08402090c867a91011f3b1f677d0
Duy Nguyen <crimethinking@users.noreply.github.com>
2026-02-25 23:03:23 +0700

    fix lints

--- snippets/time_drift_check.sh
+++ snippets/time_drift_check.sh
@@ -11,19 +11,16 @@ set -euo pipefail
 
 # Check if timedatectl is reporting unsynchronized system clock and check for PMM admin time drift status
 echo "System NTP synchronization status: $(timedatectl show -P NTPSynchronized)"
-echo "PMM time drift: $(pmm-admin status --json | jq -M '.pmm_agent_status.server_clock_drift / 1000' )" "μs"
+echo "PMM time drift: $(pmm-admin status --json | jq -M '.pmm_agent_status.server_clock_drift / 1000')" "μs"
 
 # Report back on what NTP implementation the system is running
 if systemctl is-active -q ntpd; then
-  echo "ntpd is running."
-  exit 0
+    echo "ntpd is running."
 elif systemctl is-active -q chronyd; then
-  echo "chronyd is running."
-  exit 0
+    echo "chronyd is running."
 elif systemctl is-active -q systemd-timesyncd; then
-  echo "systemd-timesyncd is running."
-  exit 0
+    echo "systemd-timesyncd is running."
 else
-  echo "Could not determine running NTP implementation or no NTP service is active."
-  exit 1
+    echo "Could not determine running NTP implementation or no NTP service is active."
+    exit 1
 fi
