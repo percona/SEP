7a04e91fa870ed38cfd753a5220398c25a5f862b
Duy Nguyen <crimethinking@users.noreply.github.com>
2026-04-01 11:33:16 +0700

    initialize service orchestrator tests

---
+++ tests/app/sep/plugins/report/test_service.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2026 Percona LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""Define tests for the app.sep.plugins.report.service module."""
+
+from unittest.mock import AsyncMock, patch
+
+import pytest
+
+from app.sep.clients.pmm import PMMRemoteAPI
+
+
+@pytest.fixture
+def pmm_api() -> AsyncMock:
+    """Return a mock PMMRemoteAPI."""
+    return AsyncMock(spec=PMMRemoteAPI)
+
+
