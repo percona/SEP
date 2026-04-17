bf94ee336d41aa820a9272d4c68c7169a8e3b085
Ignacio Durand <nachodurand@gmail.com>
2026-04-17 14:21:14 -0300

    SEP-945: Update MUI dependencies to latest versions, test fix for Mui button color variants

--- frontend/packages/shell/src/theme.ts
+++ frontend/packages/shell/src/theme.ts
@@ -3,36 +3,33 @@
 // SEP brand (purple/yellow), AppBar override, and technology palette.
 
 // Example of how to customize the theme further with overrides and/or new tokens (this is for debugging purposes, do not delete):
-/*
-import { typographyClasses } from '@mui/material/Typography';
-import { sepThemeOptions as sepThemeOptionsOriginal } from '@percona/percona-ui';
-import { PaletteMode, ThemeOptions } from '@mui/material';
 
-import { deepmerge } from "@mui/utils";
+// import { typographyClasses } from '@mui/material/Typography';
+import { sepThemeOptions as sepThemeOptionsOriginal } from '@percona/percona-ui';
+import type { PaletteMode, ThemeOptions } from '@mui/material';
+
+import { deepmerge } from '@mui/utils';
 
 const sepThemeOptions = (mode: PaletteMode): ThemeOptions => {
-
   const newOptions: ThemeOptions = {
     components: {
-      MuiDrawer: {
+      MuiButton: {
         styleOverrides: {
-          root: {
-            [`.${typographyClasses.root}`]: {
-              fontSize: "0.875rem",
-            },
-          },
+          containedSuccess: { color: '#fff' },
+          containedError: { color: '#fff' },
+          containedWarning: { color: '#fff' },
+          containedInfo: { color: '#fff' },
         },
       },
-    }
-  }
+    },
+  };
 
   return deepmerge<ThemeOptions>(sepThemeOptionsOriginal(mode), newOptions);
-}
+};
 export { sepThemeOptions };
-*/
 
 export {
-  sepThemeOptions,
+  // sepThemeOptions,
   sepPrimaryLight,
   sepPrimaryDark,
   sepTechnologyColors,
