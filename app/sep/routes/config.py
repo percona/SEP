340dbe7af5cea08907d4e3d51a5df267eca11f7b
Duy Nguyen <crimethinking@users.noreply.github.com>
2026-04-22 10:02:36 +0700

    initialize config.py

---
+++ app/sep/routes/config.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2026 Percona LLC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""Define an admin-only endpoint that exports the effective SEP configuration.
+
+The endpoint dumps every loaded Pydantic settings instance, redacts sensitive
+fields, and serves the result as a downloadable YAML file so administrators
+can audit, back up, or reuse the running configuration.
+"""
