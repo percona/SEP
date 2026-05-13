8ca51289c84380afac5574f57b819a2fdb9d349d
cortiz-percona <cesar.arteaga@percona.com>
2026-05-13 15:22:18 +0000

    clean up Vite config warnings

--- frontend/packages/shell/vite.config.ts
+++ frontend/packages/shell/vite.config.ts
@@ -16,13 +16,13 @@
  */
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   resolve: {
+    tsconfigPaths: true,
     dedupe: [
       'react',
       'react-dom',
@@ -63,6 +63,7 @@ export default defineConfig({
     noExternal: ['@percona/percona-ui'],
   },
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: (id: string) => {
