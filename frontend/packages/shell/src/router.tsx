16e8fddd646e0c65ae9a2cf87c2ad152a1cc3061
Ignacio Durand <nachodurand@gmail.com>
2026-06-09 16:02:03 -0300

    Remove unused PlaceholderPage import in shell router

--- frontend/packages/shell/src/router.tsx
+++ frontend/packages/shell/src/router.tsx
@@ -24,7 +24,6 @@ import { useAuth } from './contexts/auth';
 
 const DashboardPage = lazy(() => import('./pages/DashboardPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
-const PlaceholderPage = lazy(() => import('./pages/PlaceholderPage'));
 const SettingsPage = lazy(() => import('./pages/SettingsPage'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
